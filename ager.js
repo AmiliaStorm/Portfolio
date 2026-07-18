e1cf3cd1ebbf69a4cacdc0eb2e9be955f5131102
AmiliaStorm <96992381+AmiliaStorm@users.noreply.github.com>
2026-07-18 12:01:00 +0200

    Update ager.js

--- ager.js
+++ ager.js
@@ -1,1 +1,25 @@
+const tabs = document.querySelectorAll(".tab");
+const snippets = document.querySelectorAll(".snippet");
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    tabs.forEach((item) => {
+      item.classList.remove("active");
+    });
+
+    snippets.forEach((snippet) => {
+      snippet.classList.remove("active");
+    });
+
+    tab.classList.add("active");
+
+    const selectedSnippet = document.getElementById(
+      tab.dataset.tab
+    );
+
+    if (selectedSnippet) {
+      selectedSnippet.classList.add("active");
+    }
+  });
+});
 
